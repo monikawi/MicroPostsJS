6d4cc0099b1263bc7b660ec9fe50d36ed7e9a940
monikawi <44977764+monikawi@users.noreply.github.com>
2019-03-13 22:13:18 +0000

    src

---
+++ src files/httpsrc.js
@@ -0,0 +1,59 @@
+/**
+* EasyHTTP Library
+* Library for making HTTP requests
+*
+* @version 3.0.0
+*
+**/
+
+class EasyHTTP {
+  async get(url) {
+    const response = await fetch(url);
+    const responseData = await response.json();
+    return responseData;
+  }
+
+
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      }, 
+      body: JSON.stringify(data)
+    });
+
+    const responseData = await response.json();
+    return responseData;
+  }
+
+
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      }, 
+      body: JSON.stringify(data)
+    });
+
+    const responseData = await response.json();
+    return responseData;
+  }
+
+
+  async delete(url) {
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json'
+      }, 
+    });
+
+    const responseData = await 'Resource deleted..';
+    return responseData;
+  }
+}
+
+
+export const http = new EasyHTTP();
